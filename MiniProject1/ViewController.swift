a98e6206edc287a4f61d9d26c3c945354749683f
dev-dinx <xind0210@gmail.com>
2021-05-20 19:16:32 +0900

    Autolayout

---
+++ MiniProject1/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  MiniProject1
+//
+//  Created by 신동환 on 2021/05/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
